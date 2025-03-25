13265bfd31f6631982a6b64ec3cfd640b6e76f7a
William Torres <contatowilliamtorres@gmail.com>
2025-03-24 22:26:22 -0300

    fix: Resolve conflitos da main.c Readiciona return da função Apaga variáveis e interação com usuário para pedir valores digitados. Adiciona return e tipo de retorno da função soma.

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 
+float soma(float *a, float *b) {
+  return *a + *b;
+}
+
 float divisao (float a, float b){
   return a / b;
 }
@@ -16,9 +20,6 @@ int fatorial(int num) {
 }
 
 int main() {
-  float num1, num2;
-  float resultado;
- 
   /*
     Crie uma calculadora que peça ao usuário a operação que deseja realizar.
       A calculadora não deve ser encerrada até que o usuário concorde.
@@ -28,24 +29,9 @@ int main() {
       - Multiplicação
       - Divisão
       - Exponenciação
-      - Raíz quadrada
+      - Raiz quadrada
       - Fatorial (só para números inteiros positivos)
       - Logaritmo natural (só para valores positivos)
   */
- // calcule a divisão entre dois números 
- printf ("Escreva dois numeros.\n");
- scanf ("%f, %f", &num1, &num2);
-
-   // Verificar divisão por zero
-   if (num2 == 0) {
-    printf("Erro: Divisão por zero!\n");
-    return 1;
-   }
-
- resultado = divisao (num1, num2);
-// exiba o resultado 
-
- printf ("resultado:%.2f\n", resultado);
-
     return 0;
 }
